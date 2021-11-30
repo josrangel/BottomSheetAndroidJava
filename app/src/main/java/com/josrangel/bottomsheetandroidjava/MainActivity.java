190c76d043607c663356ad11c5f90b326c71d909
josrangel <josrangel@hotmail.com>
2021-11-29 23:12:31 -0600

    first commit

---
+++ app/src/main/java/com/josrangel/bottomsheetandroidjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.josrangel.bottomsheetandroidjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
